38f1b5a24a073a8ff859340a7361c5547f185cae
KAYRAHxWOLF <dakiesemble@gmail.com>
2024-02-04 21:47:45 +0100

    day1 done

---
+++ index.js
@@ -0,0 +1,51 @@
+// ! JAVASCRIPT !
+
+//  singleine comment
+
+/*
+    multiline comment
+*/
+
+//basicly a console printer
+console.log('hi!')
+
+//Arguments in console.log
+console.log('1', '2', '3')
+
+//Arithmetics
+console.log(11 + 58) // Addition
+console.log(70 - 1) // Subtraction
+console.log(3 / 3) // Division
+console.log(5 * 5) // Multiplication
+console.log(3 ** 2) // Exponentiation
+console.log(50 % 2) // Modulus - finding remainder
+
+// data types
+var String = 'asdasd'
+var int = 43242342
+var short = 35
+var long = 234234
+var decimal = 0.34234
+var float = 1.35436567
+var boolean = false
+var boolean = true
+let thisname = null
+/*
+
+ var: a huge mf does sooo many fuckin errors. its basicly the drama error queen
+ let: For a variable that changes at a different time, we use let.
+ const: If the data does not change at all, we use const
+
+
+ A valid JavaScript variable name must follow the following rules:
+    - A JavaScript variable name should not begin with a number.
+    - A JavaScript variable name does not allow special characters except dollar sign and underscore.
+    - A JavaScript variable name follows a camelCase convention.
+    - A JavaScript variable name should not have space between words.
+
+*/
+
+// checking data types
+console.log(typeof 4324242)
+console.log(typeof null)
+console.log(typeof undefined)
